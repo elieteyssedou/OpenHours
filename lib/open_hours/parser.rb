7c798b37fa7dee430c639decb0cf715f126f6e8c
Elie Teyssedou <elieteyssedou@gmail.com>
2016-02-19 19:26:03 +0100

    Update rules'

--- lib/open_hours/parser.rb
+++ lib/open_hours/parser.rb
@@ -15,8 +15,11 @@ module OpenHours
     protected
 
     def recognize_syntax(data_string)
-      if /24\/7/.match(data_string)
-        return :full
+      case
+      when data_string.match(/24\/7/)
+        :full
+      when data_string.match(/(\A|;)(Mo|Tu|We|Th|Fr|Sa|Su)-(Mo|Tu|We|Th|Fr|Sa|Su)( |)\d{1,2}:\d{1,2}-\d{1,2}:\d{1,2}(\A|;)/)
+        :single_rule
       end
     end
 
@@ -27,6 +30,25 @@ module OpenHours
 
     def format_output(input)
       {periods: {rules: [input], dates: "01/01:31/12"}}
+    end
+
+    def day_str_to_num(str)
+      case str
+      when "Su"
+        0
+      when "Mo"
+        1
+      when "Tu"
+        2
+      when "We"
+        3
+      when "Th"
+        4
+      when "Fr"
+        5
+      when "Sa"
+        6
+      end
     end
   end
 end
